d1d6961b508f457c466892ae26188d294607319d
XiongYuSong <937724308@qq,com>
2019-06-01 10:05:31 +0800

    增加个人中心页面

---
+++ src/main/java/hue/edu/xiong/volunteer_travel/controller/UserCenterController.java
@@ -0,0 +1,18 @@
+package hue.edu.xiong.volunteer_travel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author Xiong YuSong
+ * @Date 2019/6/1
+ */
+@Controller
+@RequestMapping("user")
+public class UserCenterController {
+
+    @RequestMapping("/centerUI")
+    public String index(){
+        return "center/user-center";
+    }
+}
