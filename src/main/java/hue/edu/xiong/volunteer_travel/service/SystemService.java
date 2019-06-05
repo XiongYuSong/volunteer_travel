b662d2f5b33d43906669d3075623c6d3001d6244
XiongYuSong <937724308@qq.com>
2019-06-05 19:10:41 +0800

    6-5

--- src/main/java/hue/edu/xiong/volunteer_travel/service/SystemService.java
+++ src/main/java/hue/edu/xiong/volunteer_travel/service/SystemService.java
@@ -5,24 +5,25 @@ import hue.edu.xiong.volunteer_travel.core.ResultGenerator;
 import hue.edu.xiong.volunteer_travel.core.ServiceException;
 import hue.edu.xiong.volunteer_travel.enums.StatusEnum;
 import hue.edu.xiong.volunteer_travel.model.Hotel;
+import hue.edu.xiong.volunteer_travel.model.SysUser;
 import hue.edu.xiong.volunteer_travel.model.User;
 import hue.edu.xiong.volunteer_travel.repository.HotelRepository;
+import hue.edu.xiong.volunteer_travel.repository.SysUserRepository;
 import hue.edu.xiong.volunteer_travel.repository.UserRepository;
+import hue.edu.xiong.volunteer_travel.util.CookieUitl;
 import hue.edu.xiong.volunteer_travel.util.IdGenerator;
-import org.codehaus.groovy.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.thymeleaf.util.DateUtils;
 import org.thymeleaf.util.StringUtils;
 
-import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Predicate;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -33,19 +34,48 @@ import java.util.List;
 public class SystemService {
 
     @Autowired
+    private SysUserRepository sysUserRepository;
+
+    @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private HotelRepository hotelRepository;
 
-    public Result login(User user, HttpServletResponse response) {
-        return null;
+    public Result login(SysUser sysUser, HttpServletResponse response) {
+
+
+        SysUser sysUserByUsrname = sysUserRepository.findSysUserByUsername(sysUser.getUsername());
+        if (sysUserByUsrname == null){
+             return ResultGenerator.genFailResult("用户名错误！");
+        }else{
+            if (sysUser.getPassword().equals(sysUserByUsrname.getPassword())){
+                CookieUitl.set(response,"username",sysUser.getUsername(),3600);
+                return ResultGenerator.genSuccessResult();
+            }else{
+                return  ResultGenerator.genFailResult("密码错误");
+            }
+        }
+
+    }
+
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        Cookie cookie = CookieUitl.get(request, "username");
+        if (cookie != null) {
+            CookieUitl.set(response, "username", null, 0);
+        }
     }
 
     public Page<User> getUserPage(Pageable pageable) {
-        Page<User> userPage = userRepository.findAll(pageable);
+        Page<User> userPage = userRepository.findAll((root,query,cb) ->{
+            List<Predicate> predicates = new ArrayList<>();
+            query.where(predicates.toArray(new Predicate[]{}));
+            query.orderBy(cb.desc(root.get("id")));
+            return null;
+        },pageable);
         return userPage;
     }
+
 
     public Page<Hotel> getHotelPage(Pageable pageable) {
         Page<Hotel> hotelPage = hotelRepository.findAll((root, query, cb) -> {
