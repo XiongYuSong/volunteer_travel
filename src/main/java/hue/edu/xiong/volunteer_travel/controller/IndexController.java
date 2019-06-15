34d70fe25028a7d86271b972ca89637dc1dbac63
XiongYuSong <937724308@qq,com>
2019-06-15 18:34:24 +0800

    6-15

--- src/main/java/hue/edu/xiong/volunteer_travel/controller/IndexController.java
+++ src/main/java/hue/edu/xiong/volunteer_travel/controller/IndexController.java
@@ -1,7 +1,14 @@
 package hue.edu.xiong.volunteer_travel.controller;
 
+import hue.edu.xiong.volunteer_travel.model.Attractions;
+import hue.edu.xiong.volunteer_travel.model.Hotel;
+import hue.edu.xiong.volunteer_travel.service.ReserveService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * @Author Xiong YuSong
@@ -9,8 +16,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class IndexController {
+
+    @Autowired
+    private ReserveService reserveService;
+
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){
+        List<Hotel> top10Hotel = reserveService.getTop10Hotel();
+        List<Attractions> top10Attractions = reserveService.getTop10Attractions();
+        model.addAttribute("top10Hotel", top10Hotel);
+        model.addAttribute("top10Attractions", top10Attractions);
         return "index";
     }
 }
